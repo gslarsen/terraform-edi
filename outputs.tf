9755a0a820368d1ec09a58b2e4b7e59c1ec0c1d6
gslarsen <54420347+gslarsen@users.noreply.github.com>
2023-04-21 18:28:57 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "api_redeployment_triggers_sha" {
   description = "api redeployment triggers sha"
   value       = aws_api_gateway_deployment.edi.triggers.redeployment
 }
+
+output "total_count_of_resources" {
+  description = "total count of resources"
+  value       = length(jsondecode(file("terraform.tfstate.backup")).resources)
+}
